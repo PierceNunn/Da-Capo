7b2b08f1c97a625f4e63c4a4a850852d6185d694
PierceNunn <agentp5151@gmail.com>
2024-03-20 12:46:23 -0500

    NoteDisplayer now changes note height
    the Y coordinate of displayed notes now depends on where on the scale the note itself is

--- Assets/Scripts/NoteDisplayer.cs
+++ Assets/Scripts/NoteDisplayer.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class NoteDisplayer : MonoBehaviour
 {
     [SerializeField] private GameObject _lastNoteDisplay;
+    [SerializeField] private float _baseHeight;
+    [SerializeField] private float _noteSpacingY;
     void Update()
     {
         IndividualNoteChart[] surroundingNotes = RhythmController.instance.GetSurroundingNotes();
         gameObject.GetComponent<SpriteRenderer>().sprite = surroundingNotes[1].Note.NoteSprite;
         _lastNoteDisplay.GetComponent<SpriteRenderer>().sprite = surroundingNotes[0].Note.NoteSprite;
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, 
+            _baseHeight + (surroundingNotes[1].PitchHeightModifier() * _noteSpacingY), gameObject.transform.position.z);
+        _lastNoteDisplay.transform.position = new Vector3(_lastNoteDisplay.transform.position.x,
+            _baseHeight + (surroundingNotes[0].PitchHeightModifier() * _noteSpacingY),
+            _lastNoteDisplay.transform.position.z);
     }
 }
--- Assets/Scripts/SerializedClasses/IndividualNoteChart.cs
+++ Assets/Scripts/SerializedClasses/IndividualNoteChart.cs
@@ -21,4 +21,27 @@ public class IndividualNoteChart
 
     public NoteTemplate Note { get => _note; set => _note = value; }
     public possiblePitches Pitch { get => _pitch; set => _pitch = value; }
+
+    public int PitchHeightModifier()
+    {
+        switch (_pitch)
+        {
+            case (possiblePitches.A):
+                return 0;
+            case (possiblePitches.B):
+                return 1;
+            case (possiblePitches.C):
+                return 2;
+            case (possiblePitches.D):
+                return 3;
+            case (possiblePitches.E):
+                return 4;
+            case (possiblePitches.F):
+                return 5;
+            case (possiblePitches.G):
+                return 6;
+            default:
+                return 0;
+        }
+    }
 }
