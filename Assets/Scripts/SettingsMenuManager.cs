4d5e52b9d553c26615c0e9709e96e033c7b706fd
PierceNunn <agentp5151@gmail.com>
2024-05-05 04:47:14 -0500

    Added volume sliders/values

--- Assets/Scripts/SettingsMenuManager.cs
+++ Assets/Scripts/SettingsMenuManager.cs
@@ -16,14 +16,20 @@ public class SettingsMenuManager : MonoBehaviour
 {
     [SerializeField] private Toggle _fourButtonToggle;
     [SerializeField] private Toggle _oneButtonToggle;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _sfxSlider;
+    [SerializeField] private AudioSource _exampleMusic;
+    [SerializeField] private AudioSource _exampleSFX;
 
     /// <summary>
-    /// Sets the status of the menu toggles to match the saved preferences.
+    /// Sets the status of the menu options to match the saved preferences.
     /// </summary>
     private void Start()
     {
         _fourButtonToggle.isOn = PlayerPrefs.GetInt("fourButtonMode", 0) == 1;
         _oneButtonToggle.isOn = PlayerPrefs.GetInt("oneButtonMode", 0) == 1;
+        _musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 0.5f);
+        _sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
     }
 
     /// <summary>
@@ -43,5 +49,26 @@ public class SettingsMenuManager : MonoBehaviour
     {
         PlayerPrefs.SetInt("oneButtonMode", _oneButtonToggle.isOn ? 1 : 0);
         PlayerPrefs.Save();
+    }
+
+    /// <summary>
+    /// Sets the value of musicVolume in PlayerPrefs to value of slider.
+    /// </summary>
+    public void SetMusicVolume()
+    {
+        PlayerPrefs.SetFloat("musicVolume", _musicSlider.value);
+        PlayerPrefs.Save();
+        _exampleMusic.volume = _musicSlider.value;
+    }
+
+    /// <summary>
+    /// Sets the value of SFXVolume in PlayerPrefs to value of slider.
+    /// </summary>
+    public void SetSFXVolume()
+    {
+        PlayerPrefs.SetFloat("SFXVolume", _sfxSlider.value);
+        PlayerPrefs.Save();
+        _exampleSFX.volume = _sfxSlider.value;
+        _exampleSFX.Play();
     }
 }
